fff88728ac1251d8b4a07d01b28887af5af8c72b
qiyomovalaziz-art <qiyomovalaziz@gmail.com>
2025-10-28 18:51:40 +0500

    Update auto_spam_bot.py

--- auto_spam_bot.py
+++ auto_spam_bot.py
@@ -1,8 +1,9 @@
-import asyncio
 from aiogram import Bot, Dispatcher, executor, types
+import asyncio
+import os
 
-API_TOKEN = '8246546890:AAHBwTmRyEgjqpEY4otaQIoTFGh3VUq-YYQ'  # BotFather dan olingan token
-INTERVAL = 2  # soniyada (har 2 soniyada xabar yuboradi)
+API_TOKEN = os.getenv("8246546890:AAHBwTmRyEgjqpEY4otaQIoTFGh3VUq-YYQ")
+INTERVAL = 2  # soniyada
 
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
@@ -15,19 +16,16 @@ is_running = False
 async def start(msg: types.Message):
     global target_chat_id
     target_chat_id = msg.chat.id
-    await msg.answer("Salom! Menga bir marta matn yubor, men uni har 2 soniyada yuborib turaman.")
+    await msg.answer("Salom! Menga matn yubor, men uni har 2 soniyada yuborib turaman.")
 
 @dp.message_handler()
 async def get_message(msg: types.Message):
     global user_message, is_running, target_chat_id
-
     if target_chat_id is None:
         target_chat_id = msg.chat.id
-
     user_message = msg.text
     is_running = True
-    await msg.answer(f"✅ Xabar qabul qilindi: '{user_message}'\nHar 2 soniyada yuborishni boshlayman.")
-
+    await msg.answer(f"✅ Xabar qabul qilindi: {user_message}")
     while is_running:
         try:
             await bot.send_message(target_chat_id, user_message)
